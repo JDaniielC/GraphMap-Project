e25c10735073f300e5df018270131a6288be72fa
José Daniel <jdsc2@cin.ufpe.br>
2022-01-06 00:24:54 -0300

    Rotas criadas porém tem muito o que fazer ainda.

---
+++ web/src/routes.js
@@ -0,0 +1,14 @@
+import MainPage  from './components/main'
+import MapPage  from './components/mapPage'
+import FlowPage  from './components/reactFlow'
+import { Routes, Route }  from 'react-router-dom'
+
+export default function Rotas() {
+  return (
+    <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route exact path="/map" element={<MapPage />} />
+        <Route exact path="/flow" element={<FlowPage />} />
+    </Routes>
+  );
+}
